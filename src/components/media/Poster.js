4bca1eb05f66739f82b80fb98939e7aed39f7339
dizrig1-star <dizrig1@gmail.com>
2026-08-20 19:53:48 -0400

    Update Poster.js

--- src/components/media/Poster.js
+++ src/components/media/Poster.js
@@ -1,3 +1,6 @@
-export function Poster(title, className=""){
+export function Poster(title, className="", posterUrl=""){
+  if(posterUrl){
+    return `<div class="poster ${className} has-image" role="img" aria-label="${title} poster"><img src="${posterUrl}" alt="${title} poster" loading="lazy"></div>`;
+  }
   return `<div class="poster ${className}" role="img" aria-label="${title} poster">${title}</div>`;
 }
