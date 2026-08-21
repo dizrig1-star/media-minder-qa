fafd911d93fa38c10a7049154d67932236142db3
dizrig1-star <dizrig1@gmail.com>
2026-08-21 08:23:10 -0400

    Refine header layout around approved brand mark

--- src/src/components/layout/Header.js
+++ src/src/components/layout/Header.js
@@ -2,8 +2,7 @@ export function Header(){
   const logo=new URL("../../../assets/branding/mm-logo.svg", import.meta.url).href;
   return `<header class="site-header">
     <div class="header-inner">
-      <a href="#landing" aria-label="Media Minder home"><img class="brand-mark" src="${logo}" alt="Media Minder"></a>
-      <div class="header-tagline">Your guide to what's worth watching</div>
+      <a class="brand-link" href="#landing" aria-label="Media Minder home"><img class="brand-mark" src="${logo}" alt="Media Minder"></a>
     </div>
   </header>`;
 }
