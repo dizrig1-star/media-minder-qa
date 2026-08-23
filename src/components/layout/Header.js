87f3d86778449f0d0c493d84553af2980c869933
dizrig1-star <dizrig1@gmail.com>
2026-08-23 08:58:23 -0400

    Update Header.js

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,5 +1,5 @@
 export function Header(){
-  const logo=new URL("../../../assets/branding/mm-log~1.svg", import.meta.url).href;
+  const logo = new URL('../../../assets/branding/approved/mm-logo-header.svg', import.meta.url).href;
   return `<header class="site-header">
     <div class="header-inner">
       <a href="#landing" aria-label="Media Minder home"><img class="brand-mark" src="${logo}" alt="Media Minder"></a>
