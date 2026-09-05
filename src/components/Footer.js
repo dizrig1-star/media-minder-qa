537c4c8c590d08c9f32df1a57ccfc955c786def9
dizrig1-star <dizrig1@gmail.com>
2026-09-04 20:31:01 -0400

    Delete src/components/Footer.js

--- src/components/Footer.js
+++
@@ -1,7 +0,0 @@
-export function Footer(){
-  return `<footer class="site-footer"><div class="footer-inner">
-    <span>Media Minder · Beta</span><span>Entertainment is the star.</span>
-  </div>
-  <button type="button" class="btn secondary return-to-menu" data-return-to-menu>&uarr; Return to Menu</button>
-  </footer>`;
-}
