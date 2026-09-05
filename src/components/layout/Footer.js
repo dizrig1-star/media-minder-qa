9021cf8ade8ed7e58dc9acb14be5e4ab97baefc5
dizrig1-star <dizrig1@gmail.com>
2026-09-04 20:30:11 -0400

    Update Footer.js

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,5 +1,7 @@
 export function Footer(){
   return `<footer class="site-footer"><div class="footer-inner">
     <span>Media Minder · Beta</span><span>Entertainment is the star.</span>
-  </div></footer>`;
+  </div>
+  <button type="button" class="btn secondary return-to-menu" data-return-to-menu>&uarr; Return to Menu</button>
+  </footer>`;
 }
